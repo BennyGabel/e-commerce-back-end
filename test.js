2b584f3c89c2cf6b28670c9d08e206a8f1440adf
13 Miles <bennygabel@gmail.com>
2022-11-17 09:50:36 -0500

    Setting models up

---
+++ test.js
@@ -0,0 +1,14 @@
+// const { INTEGER } = require("sequelize")
+
+// var nDecimal = 10.1
+// console.log( nDecimal)
+// console.log( parseInt(nDecimal))
+
+// console.log( nDecimal==parseInt(nDecimal) )
+
+// console.log( (nDecimal-INTEGER(nDecimal))!=0) 
+
+string ="AB"
+num1 = 1234
+console.log(typeof string)
+console.log(typeof num1)
